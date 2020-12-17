d1832453570d5b197dbc85c65d04e8ff0ec65018
Beka Grdzelishvili <beka.grdzelishvili@kiu.edu.ge>
2020-12-17 23:20:07 +0100

    Exercise-Template pushed by Artemis

---
+++ src/fop/w11dining/BubbleSort.java
@@ -0,0 +1,16 @@
+package fop.w11dining;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    /**
+     * Sortes dates with BubbleSort.
+     *
+     * @param input the List of Dates to be sorted
+     */
+    public void performSort(final List<Date> input) {
+
+        //TODO: implement
+    }
+}
---
+++ src/fop/w11dining/MergeSort.java
@@ -0,0 +1,17 @@
+package fop.w11dining;
+
+import java.util.*;
+
+public class MergeSort {
+
+    /**
+     * Sortes dates with MergeSort.
+     *
+     * @param input the List of Dates to be sorted
+     */
+    public void performSort(final List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
